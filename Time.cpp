9f2036533565f8f53fcb226664a7c1a18e7a7620
sana-love-cs sana-love-cs <sana-ivanov1@yandex.ru>
2023-01-14 01:52:09 +0500

    UPD

--- Time.cpp
+++ Time.cpp
@@ -1,5 +1,10 @@
 #include "Time.h"
 #include <iostream>
+#include <windows.h>    // Библиотека для русской локализации
+
+SetConsoleCP(1251);         // Ввод с консоли в кодировке 1251 для русской локализации.
+SetConsoleOutputCP(1251);   // Вывод на консоль в кодировке 1251 для русской локализации.
+
 using namespace std;
 //перегрузка операции присваивания
 Time& Time::operator=(const Time& t)
@@ -56,6 +61,9 @@ Time Time::operator-(const Time& t)
 //перегрузка глобальной функции-операции ввода
 istream& operator>>(istream& in, Time& t)
 {
+	SetConsoleCP(1251);         // Ввод с консоли в кодировке 1251 для русской локализации.
+	SetConsoleOutputCP(1251);   // Вывод на консоль в кодировке 1251 для русской локализации.
+
 	cout << "Введите минуты: "; in >> t.min;
 	cout << "Введите секунды: "; in >> t.sec;
 	return in;
